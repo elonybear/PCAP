60ebe592716bfca6cac7640055952e782c7f3a03
Elon Rubin <emrubi@umich.edu>
2016-03-15 17:40:39 -0400

    updated filter for heroku use

--- public_html/filter.js
+++ public_html/filter.js
@@ -1,7 +1,7 @@
-// Change/flip active filter
-function getUrlVars(){
+// change/flip active filter
+function geturlvars(){
 	var vars = [], hash;
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	var hashes = window.location.href.slice(window.location.href.indexof('?') + 1).split('&');
 	for(var i = 0; i < hashes.length; i++){
 		hash = hashes[i].split('=');
 		vars.push(hash[0]);
@@ -11,28 +11,34 @@ function getUrlVars(){
 }
 
 var active = 0;
-var flip = getUrlVars['degree'] || 0;
+var flip = geturlvars['degree'] || 0;
 $(".filter").click(function(e){
-	e.preventDefault();
-	var file = window.location.href.substr((window.location.href.lastIndexOf('/') + 1));
+	e.preventdefault();
+	var file = window.location.href.substr((window.location.href.lastindexof('/') + 1));
 	file = file.split('?')[0];
 	var url;
+	if(location.hostname == 'localhost') {
+		url = 'http://localhost:3000/' + file + '?filter=';
+	}
+	else {
+		url = 'http://pcap-database.herokuapp.com/' + file + '?filter=';
+	}
 	if($(this).hasClass('active')){
 		var query = getUrlVars();
 		var degree = query['degree'];
 		if(degree){
 			var new_degree = (((parseInt(degree) + 180) / 180) % 2) * 180;
-			url = 'http://localhost:3000/' + file + '?filter=' + $(this).find('p').html().toLowerCase() + '&degree=' + new_degree;	
+			url += $(this).find('p').html().toLowerCase() + '&degree=' + new_degree;	
 		}
 		else if($(this).find('p').html() != 'Title'){
-			url = 'http://localhost:3000/' + file + '?filter=piece&degree=0';	
+			url += 'piece&degree=0';	
 		}
 		else{
-			url = 'http://localhost:3000/' + file + '?filter=piece&degree=180';	
+			url += 'piece&degree=180';	
 		}
 	}
 	else{
-		url = 'http://localhost:3000/' + file + '?filter=' + $(this).find('p').html().toLowerCase() + '&degree=0';
+		url += $(this).find('p').html().toLowerCase() + '&degree=0';
 	}
 	window.location.href = url;
 });
