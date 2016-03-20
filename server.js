8f742daee9474e3a0dcefbc00a4209996a05c790
Elon Rubin <emrubi@umich.edu>
2016-03-20 16:25:52 -0400

    fixed search

--- server.js
+++ server.js
@@ -83,29 +83,21 @@ app.get('/search', function(req, res){
 	var query = req.query;
 	var where = {};
 	var order = query.filter + ' ' + query.order;
-	if(query.hasOwnProperty('q') && query.q.length > 0){
-		var q = query.q.toUpperCase();
-		where = { 
-			$or: [{
-				title: {
-					$like: '%' + q + '%'
-				}},
-				{  artist: {
-					$like: '%' + q + '%'
-				}
-			}]	
-		}
-	}	
+	var q_upper = query.q.toUpperCase();
 	if(query.hasOwnProperty('user') && query.user === 'user'){
 		where.artist_crit = false;	
 	}
-
 	db.piece.findAll({
 		where: where,
 		order: order
 	}).then(function(pieces){
 		var max_rows = storage.getItemSync('max_rows');
-		res.json(pieces);	
+
+		var filtered_pieces = pieces;
+		filtered_pieces(filtered_pieces, function(piece){
+			piece.title_upper.indexOf(q_upper) > -1 || piece.artist_upper.indexOf(q_upper) > -1;
+		});
+		res.status(200).json(filtered_pieces);	
 	}, function(e){
 		res.status(500).send(e);
 	});
